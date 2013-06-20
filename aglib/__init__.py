b644d5f44193675c9d8bf6654b135a93936119a5
Artur Glavic <glavic@poweratz>
2013-06-20 09:06:04 -0400

    Add mpfit and peakfinder and rename modules

--- aglib/__init__.py
+++ aglib/__init__.py
@@ -2,4 +2,9 @@
 Module for stuff often used in scattering.
 '''
 
-__all__=['ff', 'resolution']
+from . import functions
+from .functions import *
+from .peakfinder import PeakFinder
+from .mpfit import mpfit
+
+__all__=['FF', 'res', 'mpfit', 'PeakFinder']+functions.__all__
