737bd36ec40a19e69cf56176daae9a34c381d996
Peter Willendrup <pkwi@fysik.dtu.dk>
2025-03-11 14:32:35 +0100

    Use .tolower() due to upstream McStas changes

--- aglib/mcstas_reader.py
+++ aglib/mcstas_reader.py
@@ -42,7 +42,7 @@ class HeaderFile:
     else:
       data=path.read().decode('UTF-8')
     data_lines=data.splitlines()
-    if not data.startswith('McStas simulation description file'):
+    if not data.lower().startswith('mcstas simulation description file'):
       raise IOError('Not a valid McStas description file.')
     self._data['start_time']=data_lines[1].split(':', 1)[1].strip()
     self._data['program_name']=data_lines[2].split(':', 1)[1].strip()
